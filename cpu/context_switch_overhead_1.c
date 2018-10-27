d0d7aeb17c52bd22767ec65e3a55a69ca8020b01
Anil Yelam <anilkyelam@gmail.com>
2018-10-26 19:12:42 -0700

    context switch using blocking pipes

--- cpu/context_switch_overhead_1.c
+++ cpu/context_switch_overhead_1.c
@@ -12,7 +12,7 @@
 
 
 // Rdtsc blocks
-unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
+unsigned cycles_low, cycles_high;
 
 static __inline__ unsigned long long rdtsc(void)
 {
@@ -22,26 +22,19 @@ static __inline__ unsigned long long rdtsc(void)
             "%rax", "rbx", "rcx", "rdx");
 }
 
-static __inline__ unsigned long long rdtsc1(void)
-{
-    __asm__ __volatile__ ("RDTSC\n\t"
-            "mov %%edx, %0\n\t"
-            "mov %%eax, %1\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
-            "%rax", "rbx", "rcx", "rdx");
-}
-
 int main()
 {
     int i, cpu_id, cpid, max_trials = 100;
-	int pipefd[2];
+	int p2c_pipe[2];
+	int c2p_pipe[2];
+	uint64_t time_child, time_parent, time_spent, avg_time_spent = 0, dummy_time = 0;
 
-    printf("Parent PID: %d\n", getpid());
+    // printf("Parent PID: %d\n", getpid());
 
     // Get the CPU on which the thread is running
     cpu_id = sched_getcpu();
-    printf("Parent CPU ID: %d\n", cpu_id);
 
-	// Set parent CPU affinity. It'll be inherited by the child as well.
+	// Set parent CPU affinity. It'll be inherited by the child as well - this will restrict both processes to one core.
 	cpu_set_t set;
 	CPU_ZERO(&set);
 	CPU_SET(cpu_id, &set);
@@ -54,54 +47,60 @@ int main()
 		exit(-1);
 	}
 
+	// Create pipes
+	pipe(p2c_pipe);
+	pipe(c2p_pipe);
 
-	uint64_t start, end, avg_time_spent = 0;
-	long time_spent;
-	for(i=0;i<max_trials;i++)
+
+	cpid = fork();
+	if (cpid == 0) 
 	{
-		// Create pipe
-		pipe(pipefd);
+		// Child process
+		close(c2p_pipe[0]);
+		close(p2c_pipe[1]);
 
-		cpid = fork();
-		if (cpid == 0) 
+		// First synchronization
+		write(c2p_pipe[1], &dummy_time, sizeof(dummy_time));	
+
+		for(i=0;i<max_trials;i++)
 		{
-			// Child process: collect time diff
-			rdtsc1();
-			end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
-			// time_spent = end - start;		
-			//cpu_id = sched_getcpu();
-			//printf("Child CPU ID: %d\n", cpu_id);
-			// printf("%lu\n", time_spent);
+			// Wait for parent to write
+			read(p2c_pipe[0], &dummy_time, sizeof(dummy_time));
 			
-			// Write the time diff to parent
-			close(pipefd[0]);
-			write(pipefd[1], &end, sizeof(end));
-			close(pipefd[1]);
-			
-			exit(0);
-		}
-		else
-		{
-			// Start timer
+			// Collect time and write to parent
 			rdtsc();
-
-			// Parent: Read the time diff from child (also forces a context switch)
-			close(pipefd[1]);
-			read(pipefd[0], &end, sizeof(end));
-			close(pipefd[0]);
-		
-			start = ( ((uint64_t)cycles_high << 32) | cycles_low );
-			time_spent = end - start;
-
-			// printf("%ld\n", time_spent);
-			avg_time_spent += time_spent;
-
-			// Wait for child
-			wait(NULL);
+			time_child = ( ((uint64_t)cycles_high << 32) | cycles_low );
+			// printf("C - %lu\n", time_child);
+			write(c2p_pipe[1], &time_child, sizeof(time_child));
 		}
 	}
+	else
+	{
+		// Parent process
+		close(c2p_pipe[1]);
+		close(p2c_pipe[0]);
 
-	printf("\nAvg. context switch time: %lf\n", avg_time_spent*1.0/max_trials);
+		// First synchronization
+		read(c2p_pipe[0], &dummy_time, sizeof(dummy_time));
+
+		for(i=0;i<max_trials;i++)
+		{
+			// Write to unblock child
+			write(p2c_pipe[1], &dummy_time, sizeof(dummy_time));
+			
+			// Read from child, get current time and calculate time diff
+			read(c2p_pipe[0], &time_child, sizeof(time_child));
+			rdtsc();
+			time_parent = ( ((uint64_t)cycles_high << 32) | cycles_low );
+			// printf("P - %lu\n", time_parent);
+
+			time_spent = time_parent - time_child;
+			// printf("D - %lu\n", time_spent);
+			avg_time_spent += time_spent;
+		}
+
+		printf("Parent CPU ID: %d, Avg. context switch time: %lf\n", cpu_id, avg_time_spent*1.0/max_trials);
+	}
 
     return 0;
 }
