f2b5dcef5d43d04a4d4f098482d031aecb63eea1
Audrey Randall <aurandal@b10-01.sysnet.ucsd.edu>
2018-11-18 11:22:52 -0800

    Improve page fault measurement.

--- makefile
+++ makefile
@@ -7,4 +7,7 @@ CFLAGS=-O0 -g
 measure_overhead: measure_overhead.o
 	$(CC) -o measure_overhead measure_overhead.o -lm
 
+page_fault: measure_page_fault.o
+	$(CC) -o page_fault measure_page_fault.o -lm
+
 #To run:  sudo nice --20 taskset 0x1 ./measure_overhead
--- measure_page_fault.c
+++ measure_page_fault.c
@@ -28,10 +28,10 @@ static __inline__ unsigned long long rdtsc1(void)
     __asm__ __volatile__ ("RDTSC\n\t"
             "mov %%edx, %0\n\t"
             "mov %%eax, %1\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::
-            "%rax", "rbx", "rcx", "rdx");
+			"%rax", "rbx", "rcx", "rdx");
 }
 
-void measure_page_fault(char* filename){
+int measure_page_fault(char* filename){
   // We need to try to measure a whole page fault, from attempting to access
   // the memory address to actually being able to access it. So we need to find
   // a way to ask for a new page every time.
@@ -46,27 +46,28 @@ void measure_page_fault(char* filename){
   fstat(fd, &stats);
   posix_fadvise(fd, 0, stats.st_size, POSIX_FADV_DONTNEED);
   int repetitions[stats.st_size];
-  char* page = mmap(NULL, page_size, PROT_READ, MAP_SHARED, fd, 0);
+  char* page = mmap(NULL, stats.st_size, PROT_READ, MAP_SHARED, fd, 0);
   assert(page != MAP_FAILED);
 
-  for(int i = 0; i < stats.st_size; i++) {
-    rdtsc();
-    k += page[i];
-    rdtsc1();
-    start = ( ((uint64_t)cycles_high << 32) | cycles_low );
-    end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
-    repetitions[i] = end - start;
-  }
+  rdtsc();
+  k += page[0];
+  rdtsc1();
+  start = ( ((uint64_t)cycles_high << 32) | cycles_low );
+  end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
   close(fd);
+  return end - start;
 }
 
 int main() {
   struct rusage r;
   printf("Page size: %d\n", getpagesize());
+  int reps = 50;
+  int num_faults = 0;
+  float measurements[reps];
   for(int j = 0; j < 50; j++) {
-    printf("J = %d\n", j);
-    measure_page_fault("test.txt");
+    measurements[j] = measure_page_fault("test.txt");
     getrusage(RUSAGE_SELF, &r);
-    printf("Major page faults: %ld\n", r.ru_majflt);
+	num_faults += r.ru_majflt;
   }
+  printf("Total page faults: %d\n", num_faults);
 }
